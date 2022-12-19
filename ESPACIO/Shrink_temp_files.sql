58a885ed786854ab02210bbbab71ad30969bfd79
apoguita <77995928+apoguita@users.noreply.github.com>
2022-12-19 11:00:48 -0600

    Create Shrink_temp_files.sql
    scripts de recuperación de espacio

---
+++ ESPACIO/Shrink_temp_files.sql
@@ -0,0 +1,27 @@
+ALTER TABLESPACE TEMP SHRINK SPACE;
+
+-------------------------------------------------------
+
+
+select * from v$asm_diskgroup;
+
+select * from v$tablespace
+where 
+name LIKE 'UNDO%'
+or name like '%TEMP%';
+
+select * from dba_temp_free_space;
+
+ SELECT   A.tablespace_name tablespace, D.mb_total,
+         SUM (A.used_blocks * D.block_size) / 1024 / 1024 mb_used,
+         D.mb_total - SUM (A.used_blocks * D.block_size) / 1024 / 1024 mb_free
+FROM     v$sort_segment A,
+         (
+         SELECT   B.name, C.block_size, SUM (C.bytes) / 1024 / 1024 mb_total
+         FROM     v$tablespace B, v$tempfile C
+         WHERE    B.ts#= C.ts#
+         GROUP BY B.name, C.block_size
+         ) D
+WHERE    A.tablespace_name = D.name
+GROUP by A.tablespace_name, D.mb_total;
+
