08527046cf14e5c31c0fa9fc889184b888acbf15
apoguita <77995928+apoguita@users.noreply.github.com>
2023-01-31 15:17:42 -0600

    Update Copia_Privilegios.sql

--- USUARIOS/Copia_Privilegios.sql
+++ USUARIOS/Copia_Privilegios.sql
@@ -3,10 +3,10 @@
 
 select * from dba_users where username in (:UOrigen,:UDestino);
 
-select 'ALTER USER ' || :UDestino || ' identified by "PROMERICA.2023$" ACCOUNT UNLOCK PASSWORD EXPIRE PROFILE ' || PROFILE || ';' from dba_users where username = :UOrigen;
-
-select 'GRANT ' || PRIVILEGE || ' to ' || :UDestino || ';'  from dba_sys_privs where grantee = :UOrigen;
-
-select 'GRANT ' || GRANTED_ROLE || ' to ' || :UDestino || ';' from dba_role_privs where grantee = :UOrigen;
-
+select 'ALTER USER ' || :UDestino || ' identified by "PROMERICA.2023$" ACCOUNT UNLOCK PASSWORD EXPIRE PROFILE ' || PROFILE || ';' from dba_users where username = :UOrigen
+union
+select 'GRANT ' || PRIVILEGE || ' to ' || :UDestino || ';'  from dba_sys_privs where grantee = :UOrigen
+union
+select 'GRANT ' || GRANTED_ROLE || ' to ' || :UDestino || ';' from dba_role_privs where grantee = :UOrigen
+union
 select 'GRANT ' || PRIVILEGE || ' on ' || OWNER ||  '.' || TABLE_NAME || ' to ' || :UDestino || ';'  from dba_tab_privs where owner not in ('SYS','SYSTEM') and grantee = :UOrigen;
