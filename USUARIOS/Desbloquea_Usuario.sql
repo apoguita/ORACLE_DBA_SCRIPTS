5db0fc0efe4f45d10089c9cd6f14fdcc0bca21ad
apoguita <77995928+apoguita@users.noreply.github.com>
2023-03-29 16:08:16 -0600

    Update Desbloquea_Usuario.sql

--- USUARIOS/Desbloquea_Usuario.sql
+++ USUARIOS/Desbloquea_Usuario.sql
@@ -1,7 +1,7 @@
 
 select * from dba_users where username in (:UDestino);
 
-select 'ALTER USER ' || :UDestino || ' identified by "PROMERICA.2023$" ACCOUNT UNLOCK PASSWORD EXPIRE PROFILE ' || PROFILE || ';' from dba_users where username = :UDestino;
+select 'ALTER USER ' || :UDestino || ' identified by "Promerica.2023$$" ACCOUNT UNLOCK PASSWORD EXPIRE PROFILE ' || PROFILE || ';' from dba_users where username = :UDestino;
 
 /*
 se desbloqueó y cambio contraseña al usuario, deberá cambiar contraseña en el siguiente logueo
