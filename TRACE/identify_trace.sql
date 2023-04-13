add64d15d831f65f73b14c24ba93204f7489157f
apoguita <77995928+apoguita@users.noreply.github.com>
2023-04-13 11:01:22 -0600

    Update identify_trace.sql
    identifica nombre de archivo trace de una sesion SID

--- TRACE/identify_trace.sql
+++ TRACE/identify_trace.sql
@@ -1,13 +1,8 @@
-select
-   r.value                                ||'/diag/rdbms/'||
-   sys_context('USERENV','DB_NAME')       ||'/'||
-   sys_context('USERENV','INSTANCE_NAME') ||'/trace/'||
-   sys_context('USERENV','INSTANCE_NAME') ||s.INST_ID  ||'_ora_'||p.spid||'.trc'
-   as tracefile_name
-from gv$session s, gv$parameter r, gv$process p
-where r.name = 'diagnostic_dest'
-and s.sid = &1
-and p.addr = s.paddr;
+select s.INST_ID, s.SID, s.USERNAME, s.STATUS, p.TRACEFILE from gv$session s, gv$process p
+where 
+        p.addr = s.paddr
+    and p.inst_id = s.inst_id
+    and s.sid = &SID
 
---/u01/app/oracle\diag\rdbms\CSP01T\csp01t2\trace\CSP01T_ora_12594.trc
+--1	1036	EDGARR_BS	ACTIVE	/u01/app/oracle/diag/rdbms/csp01t/csp01t1/trace/csp01t1_ora_12594.trc
 
