09b4242537ca0a0e1fa3b066cb8232e0be15324a
apoguita <77995928+apoguita@users.noreply.github.com>
2023-04-13 10:29:50 -0600

    Update identify_trace.sql

--- TRACE/identify_trace.sql
+++ TRACE/identify_trace.sql
@@ -1,10 +1,13 @@
 select
-   r.value                                ||'\diag\rdbms\'||
-   sys_context('USERENV','DB_NAME')       ||'\'||
-   sys_context('USERENV','INSTANCE_NAME') ||'\trace\'||
-   sys_context('USERENV','DB_NAME')       ||'_ora_'||p.spid||'.trc'
+   r.value                                ||'/diag/rdbms/'||
+   sys_context('USERENV','DB_NAME')       ||'/'||
+   sys_context('USERENV','INSTANCE_NAME') ||'/trace/'||
+   sys_context('USERENV','INSTANCE_NAME') ||s.INST_ID  ||'_ora_'||p.spid||'.trc'
    as tracefile_name
-from v$session s, v$parameter r, v$process p
+from gv$session s, gv$parameter r, gv$process p
 where r.name = 'diagnostic_dest'
 and s.sid = &1
 and p.addr = s.paddr;
+
+--/u01/app/oracle\diag\rdbms\CSP01T\csp01t2\trace\CSP01T_ora_12594.trc
+
